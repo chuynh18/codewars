139c8d7a910bf5d70d1b951809e34d49dc5e0e71
ch <38088640+chuynh18@users.noreply.github.com>
2018-11-21 20:28:19 -0800

    mild styling changes to answer area.  clear answer area for expression parser when user types into text entry box

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,6 +109,7 @@ const countParenthesis = function(input) {
 document.getElementById("expression").addEventListener("keyup", function(event) {
    document.getElementById("expression").value = allowedChars(document.getElementById("expression").value, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "*", "/", "(", ")", " "]);
    document.getElementById("mismatch").textContent = countParenthesis(document.getElementById("expression").value);
+   document.getElementById("expressionResult").innerHTML = "<br>";
 
    if (event.key === "Enter") {
       evalExpression();
